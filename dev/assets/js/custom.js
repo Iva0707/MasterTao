4cfc56a97497ec0e6a6053ac783c093921006dae
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-05-21 11:31:14 +0200

    first screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -6,3 +6,29 @@ function showNav(){
 }
 
 burger.addEventListener('click', showNav);
+
+Fancybox.bind("[data-fancybox]", {});
+$('.single-item').slick();
+
+const swiper = new Swiper('.swiper', {  
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: false,
+});
+
+$(document).ready(function(){
+  $('.your-class').slick({
+      dots: true,
+      infinite: false,
+      pauseOnHover: true,
+      autoplay: true,
+      autoplaySpeed: 1000,
+  });
+});
