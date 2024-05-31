ded20e7e165f877f17fc48e9db584f0c3443b539
Iva Tkachuk <ivatkachuk@MacBook---Iva.local>
2024-05-31 14:12:08 +0200

    first screen desktop

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,3 +1,25 @@
+// _____fancybox_____
+Fancybox.bind("[data-fancybox]", {});
+
+// _____slick_____
+$('.single-item').slick();
+
+// _____burger_____
+var forEach = function(t, o, r) {
+  if ("[object Object]" === Object.prototype.toString.call(t))
+      for (var c in t) Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+  else
+      for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t)
+};
+var hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function(hamburger) {
+      hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+      }, false);
+  });
+}
+
 // _____burger_____
 const burger = document.querySelector('#burger');
 const body = document.querySelector('body');
@@ -10,12 +32,6 @@ function showNav(){
 
 burger.addEventListener('click', showNav);
 
-
-// _____fancybox_____
-Fancybox.bind("[data-fancybox]", {});
-
-// _____slick_____
-$('.single-item').slick();
 
 // _____swiper_____
 const swiper = new Swiper('.slider_info', {  
